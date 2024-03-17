c8ccef647a0b520a9f47ae32755114126cbcc03e
Mukesh Loganathan <mukeshloganathan12@gmail.com>
2024-03-17 08:54:24 -0400

    form.pug

--- mini_message_board/routes/index.js
+++ mini_message_board/routes/index.js
@@ -1,23 +1,37 @@
-var express = require('express');
-var router = express.Router();
-
 const messages = [
   {
-    text: "Hi there!",
+    text: "Lets Fucking Get It!",
     user:"Armando",
     added: new Date(),
   },
 
   {
-    text: " NCAA Basketball Season Is On",
+    text: " We are so winning the ACC tournament",
     user: "RJ Davis",
     added: new Date(),
   },
 ];
 
+const testing = "Testing_IT_Out";
+const test = ['carbon', 'nitrogen', 'helium','hydrogen']
+const number = 69
+
+var express = require('express');
+var router = express.Router();
+
+const person = {
+  name: "John Doe",
+  age: 30
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: 'Hey', test: test[1], info: messages})
+  
 });
+
+router.get('/new', function(req,res,next) {
+  res.render('form')
+})
 
 module.exports = router;
